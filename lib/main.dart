b3191368f5d3a1f711ff11f4019bcd8eeb436ce5
Aladdin Sonni <aladdin.sonni@gmail.com>
2019-08-28 16:58:47 +0200

    Set folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-import './categories_screen.dart';
-import './category_meals_screen.dart';
+import 'package:meals/screens/categories_screen.dart';
+import 'package:meals/screens/category_meals_screen.dart';
 
 void main() => runApp(MyApp());
 
---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals/dummy_data.dart';
+import 'package:meals/widgets/meal_item.dart';
+
+class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals';
+  // final String categoryId;
+  // final String categoryTitle;
+
+  // CategoryMealsScreen({
+  //   this.categoryId,
+  //   this.categoryTitle,
+  // });
+
+  @override
+  Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categoryIds.contains(categoryId);
+    }).toList();
+    final categoryTitle = routeArgs['title'];
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          categoryTitle,
+        ),
+      ),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            title: categoryMeals[index].title,
+            imageUrl: categoryMeals[index].imageUrl,
+            duration: categoryMeals[index].duration,
+            complexity: categoryMeals[index].complexity,
+            affordability: categoryMeals[index].affordability,
+          );
+        },
+        itemCount: categoryMeals.length,
+      ),
+    );
+  }
+}
