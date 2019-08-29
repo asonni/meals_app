646f66f6985ad1d71296b5d5c6367632f3cc8012
Aladdin Sonni <aladdin.sonni@gmail.com>
2019-08-29 15:52:55 +0200

    Replacing Pages (Instead of Pushing)

---
+++ lib/screens/filters_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:meals/widgets/main_drawer.dart';
+
+class FiltersScreen extends StatelessWidget {
+  static const routeName = "/filters";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Your Filters',
+        ),
+      ),
+      drawer: MainDrawer(),
+      body: Center(
+        child: Text(
+          'Filters!',
+        ),
+      ),
+    );
+  }
+}
