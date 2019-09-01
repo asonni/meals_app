650dd373169ab051ae3ab03649b5bf9b48e550f4
Aladdin Sonni <aladdin.sonni@gmail.com>
2019-09-01 10:05:05 +0200

    Add const in the widget tree to improve preformance

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
-import 'package:meals/dummy_data.dart';
 import 'package:meals/screens/categories_screen.dart';
 import 'package:meals/screens/category_meals_screen.dart';
 import 'package:meals/screens/filters_screen.dart';
 import 'package:meals/screens/meal_detail_screen.dart';
 import 'package:meals/screens/tabs_screen.dart';
+import 'package:meals/dummy_data.dart';
 import 'package:meals/models/meal.dart';
 
 void main() => runApp(MyApp());
@@ -31,18 +31,10 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _filters = filterData;
       _availableMeals = DUMMY_MEALS.where((meal) {
-        if (_filters['gluten'] && !meal.isGlutenFree) {
-          return false;
-        }
-        if (_filters['lactose'] && !meal.isLactoseFree) {
-          return false;
-        }
-        if (_filters['vegan'] && !meal.isVegan) {
-          return false;
-        }
-        if (_filters['vegetarian'] && !meal.isVegetarian) {
-          return false;
-        }
+        if (_filters['gluten'] && !meal.isGlutenFree) return false;
+        if (_filters['lactose'] && !meal.isLactoseFree) return false;
+        if (_filters['vegan'] && !meal.isVegan) return false;
+        if (_filters['vegetarian'] && !meal.isVegetarian) return false;
         return true;
       }).toList();
     });
